d20457c0c937b39e5922f703e8a03e7ba273400e
Roland Teslaru <roland.teslaru@protonmail.com>
2025-09-16 16:39:16 +0300

    Websocket secure

--- packages/ZeruelDashboard/src/stores/useGatewayService.ts
+++ packages/ZeruelDashboard/src/stores/useGatewayService.ts
@@ -36,6 +36,9 @@ export const useGatewayService = create<State & Actions>()(
             if(!gatewayWebsocketUrl)
                 console.error("Public GatewayService Websocket Url has not been provided in the env")
 
+            console.log("CONNECTING TO GATEWAY WEBSOCKET AT:", gatewayWebsocketUrl)
+
+            
             const ws = new WebSocket(process.env.NEXT_PUBLIC_GATEWAY_WEBSOCKET_URL);
 
             ws.onopen = () => {
