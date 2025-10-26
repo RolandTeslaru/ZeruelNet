60a229d199043d2047aa661774627574e9ce50df
Roland Teslaru <roland.teslaru@protonmail.com>
2025-10-27 01:21:26 +0200

    gitignore

--- packages/ZeruelDashboard/src/components/pages/Trends/_components/TimelineQueryPanel.tsx
+++ packages/ZeruelDashboard/src/components/pages/Trends/_components/TimelineQueryPanel.tsx
@@ -1,25 +1,9 @@
 import React, { memo, useCallback } from 'react'
 import { useTrendsStore } from '../context'
-import { fetchComposedData } from '@/lib/api/trends'
 import { TrendsAPI } from '@/types/api'
-import { useQuery } from '@tanstack/react-query'
-import {
-    Popover, PopoverTrigger, PopoverContent,
-    Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem,
-    Button
-} from "@zeruel/shared-ui/foundations"
-import DataViewerWrapper from '@zeruel/shared-ui/DataViewerWrapper'
-import { DateRangePicker } from '@zeruel/shared-ui/foundations/DateRangePicker'
-import { Info } from '@zeruel/shared-ui/icons'
-import { DateRange } from 'react-day-picker'
-import { DummyTree, DummyTreeBranch, RenderBranchFunction } from '@zeruel/shared-ui/Tree/types'
-import Tree from '@zeruel/shared-ui/Tree'
 import { Form, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import z from 'zod'
-import JsonView from 'react18-json-view'
 import ZodFromTreeRenderer from '@/components/ZodFormTreeRenderer'
-import Search from '@zeruel/shared-ui/Search'
 
 const TimelineQueryPanel = memo(() => {
     const [composedDataParams, setComposedDataParams] = useTrendsStore(state => [
